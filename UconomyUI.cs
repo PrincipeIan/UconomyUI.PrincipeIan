9a9d90c858dcbe08a4fe249dac2bff61ca29ffe9
Ian <47759998+PrincipeIan@users.noreply.github.com>
2019-04-10 17:20:24 -0300

    Add files via upload

---
+++ UconomyUI.cs
@@ -0,0 +1,52 @@
+﻿using Rocket.Unturned;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using Steamworks;
+using fr34kyn01535.Uconomy;
+
+namespace UconomyUI.By.PrincipeIan
+{
+    public class UconomyUI : RocketPlugin<UconomyUIConfig>
+    {
+        public static UconomyUI Instance;
+        Dictionary<CSteamID, decimal> dicionaa = new Dictionary<CSteamID, decimal>();
+
+        protected override void Load()
+        {
+        Instance = this;
+        Rocket.Core.Logging.Logger.Log("Plugin loaded correctly");
+        Rocket.Core.Logging.Logger.Log("UconomyUI free By PrincipeIan");
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+            Uconomy.Instance.OnBalanceUpdate += Instance_OnBalanceUpdate;
+            U.Events.OnPlayerDisconnected += Events_OnPlayerDisconnected;
+        }
+        private void Events_OnPlayerDisconnected(UnturnedPlayer player) {
+
+            dicionaa.Remove(player.CSteamID);
+            EffectManager.askEffectClearByID(27411, player.CSteamID);
+            EffectManager.askEffectClearByID(27421, player.CSteamID);
+        }
+        private void Instance_OnBalanceUpdate(UnturnedPlayer player, decimal amt)
+        {
+            dicionaa[player.CSteamID] = Uconomy.Instance.Database.GetBalance(player.CSteamID.ToString());
+            EffectManager.askEffectClearByID(27411, player.CSteamID);
+            EffectManager.askEffectClearByID(27421, player.CSteamID);
+            EffectManager.sendUIEffect(27411, 27412, player.CSteamID, true, Configuration.Instance.colorBalance, "$" + dicionaa[player.CSteamID].ToString());
+            EffectManager.sendUIEffect(27421, 27422, player.CSteamID, true, Configuration.Instance.colorName, Configuration.Instance.serverName);
+        }
+        private void Events_OnPlayerConnected(UnturnedPlayer player)
+        {         
+            dicionaa.Add(player.CSteamID, Uconomy.Instance.Database.GetBalance(player.CSteamID.ToString()));
+            EffectManager.sendUIEffect(27411, 27412, player.CSteamID, true, Configuration.Instance.colorBalance ,"$"+dicionaa[player.CSteamID].ToString());
+            EffectManager.sendUIEffect(27421, 27422, player.CSteamID, true, Configuration.Instance.colorName, Configuration.Instance.serverName);
+        }
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+            Uconomy.Instance.OnBalanceUpdate -= Instance_OnBalanceUpdate;
+            U.Events.OnPlayerDisconnected -= Events_OnPlayerDisconnected;
+        }
+    }
+}
---
+++ UconomyUIConfig.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+
+namespace UconomyUI.By.PrincipeIan
+{
+    public class UconomyUIConfig : IRocketPluginConfiguration
+    {
+        public string colorBalance;
+        public string serverName;
+        public string colorName;
+        public void LoadDefaults()
+        {
+            colorBalance = "#06B409";
+            serverName = "ServerName";
+            colorName = "#B43E06";
+        }
+    }
+}
